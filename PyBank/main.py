a6e974de8557b0a76df0c9b983e4dfccffc8152e
musicality84 <haleyheer@Haleys-MacBook-Pro.local>
2024-01-14 17:46:41 -0700

    Progress save 2024-01-14 5:46pm

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,24 +15,24 @@ with open(csvpath) as csvfile:
     profit_decrease = 0
     profit_month = "Month"
     for row in csvreader:
-        total += float(row[1])
-        months = months + 1
-        profit_change = int(row[1]) - previous_row
-        if profit_change > profit_increase:
+        total += float(row[1]) #sums total of profit gain/loss
+        months = months + 1 #counts number of months
+        profit_change = int(row[1]) - previous_row #compares current value to previous value
+        if profit_change > profit_increase: #looks for greatest increase
             profit_increase = profit_change
             profit_month_increase = str(row[0])
-        elif profit_change < profit_decrease:
+        elif profit_change < profit_decrease: #looks for greatest decrease
             profit_decrease = profit_change
             profit_month_decrease = str(row[0])
-        profit_change_record.append(profit_change)
-        previous_row = int(row[1])
+        profit_change_record.append(profit_change) #places profit change in an array
+        previous_row = int(row[1]) #sets previous row for next iteration
     length = len(profit_change_record)
     average_change = sum(profit_change_record)/length
-    average_change = round(average_change,2)
+    average_change = round(average_change,2) #calculates average and rounds to 2 decimal places
 print("Financial Analysis")
 print("===================")
-print(f"Total Months: {months}")
-print(f"Total: {total}")
+print(f"Total Months: {months+1}") #adds a month since the for loop started on row 2
+print(f"Total: {total+int(first_row[1])}") #adds the skipped first row back to the total
 print(f"Average Change:{average_change}")
 print(f"Greatest increase in profits: {profit_month_increase} {profit_increase}")
 print(f"Greatest decrease in profits: {profit_month_decrease} {profit_decrease}")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,9 +6,37 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader) #skip header
     #establish variables
     total = 0
+    candidates = []
+    candidates_unique = []
+    candidate_count = []
+    first_row = next(csvreader)
+    first_row_candidate = str(first_row[2])
     for row in csvreader:
         total = total + 1
+        if str(row[2])!=first_row_candidate:
+            candidates.append(first_row_candidate)
+            first_row_candidate = str(row[2])
+            candidate_count.append(str(row[2]))
+            for name in candidates:
+                if name not in candidates_unique:
+                    candidates_unique.append(name)
+        else:
+            candidate_count.append(str(row[2]))
+    candidate_1_votes = candidate_count.count(candidates_unique[0])+1 #adds plus one since we skipped the first row
+    candidate_2_votes = candidate_count.count(candidates_unique[1])
+    candidate_3_votes = candidate_count.count(candidates_unique[2])
+    candidate_1_percent = candidate_1_votes/(candidate_1_votes+candidate_2_votes+candidate_3_votes)*100
+    candidate_1_percent = round(candidate_1_percent,3)
+    candidate_2_percent = candidate_2_votes / (candidate_1_votes + candidate_2_votes + candidate_3_votes) * 100
+    candidate_2_percent = round(candidate_2_percent, 3)
+    candidate_3_percent = candidate_3_votes / (candidate_1_votes + candidate_2_votes + candidate_3_votes) * 100
+    candidate_3_percent = round(candidate_3_percent, 3)
 print("Election Results")
 print("-----------------")
-print(f"Total: {total}")
+print(f"Total Votes: {total+1}")#adds the skipped row 1 back to the count
 print("-----------------")
+print(f"{candidates_unique[0]}: {candidate_1_percent}% ({candidate_1_votes})")
+print(f"{candidates_unique[1]}: {candidate_2_percent}% ({candidate_2_votes})")
+print(f"{candidates_unique[2]}: {candidate_3_percent}% ({candidate_3_votes})")
+print("-----------------")
+print(f"Winner: {candidates_unique[1]}")
